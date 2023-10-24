44029efac9399f30bc371db37ca6da5b2590f30a
Irvin Dominguez <al169890@alumnos.uacj.mx>
2023-10-24 00:09:24 -0600

    login funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './paginas/login/login.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+ {path:'login',component:LoginComponent},
+ {path:'inicio',component:InicioComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
+//ANGULAR MODULES
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+//COMPONENTES
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './paginas/login/login.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+
+
+//PRIMENG MODULES
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { AppRoutingModule } from './app-routing.module';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+    
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
